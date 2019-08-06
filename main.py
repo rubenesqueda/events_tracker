0184572e57d1497b720e00e0d3e7145656f7b2f3
Ubuntu <ubuntu@ip-172-31-93-52.ec2.internal>
2019-08-06 23:05:25 +0000

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
