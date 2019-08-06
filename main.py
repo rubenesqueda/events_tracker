08c822c37151407f7c98363bf7f41ca89c1eaf99
Ubuntu <ubuntu@ip-172-31-95-176.ec2.internal>
2019-08-06 23:27:38 +0000

    .

--- main.py
+++ main.py
@@ -8,3 +8,70 @@ the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+    
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):
+        
+        welcome_template = the_jinja_env.get_template('templates/index.html')
+        self.response.write(welcome_template.render())
+    
+class Upcoming(webapp2.RequestHandler):
+    
+    
+    
+    
+    
+class AddEvents(webapp2.RequestHandler):
+    
+    
+    
+class Map(webapp2.RequestHandler):
+    
+    
+
+
+class Calendar(webapp2)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+app = webapp2.WSGIApplication([
+    ('/', EnterInfoHandler)
+], debug=True)
