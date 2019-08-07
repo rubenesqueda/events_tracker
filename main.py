954528e802b3b0760405669d4e661c1940b2d453
Ubuntu <ubuntu@ip-172-31-93-52.ec2.internal>
2019-08-07 18:44:52 +0000

    New

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import webapp2
 import jinja2
 import os
-#from models import Meme, CssiUser ////importatn put our model EventUser
+from models import Event, CssiUser 
 from google.appengine.api import users
 
 
@@ -16,7 +16,7 @@ def checkLoggedInAndRegistered(request):
     user = users.get_current_user()
         
     if not user: 
-        request.redirect("/login") #ask to login,if not redirect 
+        request.redirect("/sign_in") #ask to login,if not redirect 
         return
         welcome_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(welcome_template.render())
@@ -33,11 +33,11 @@ def checkLoggedInAndRegistered(request):
     # Check if user is registered
        
     email_address = user.nickname()
-    registered_user = CssiUser.query().filter(CssiUser.email == email_address).get() #EventUser
+    registered_user = Event_User.query().filter(Event_User.email == email_address).get() 
     
 
     if not registered_user:
-         request.redirect("/register") #useful if not registered goes to rdr
+         request.redirect("/signup") #useful if not registered goes to rdr
          return 
 
     
@@ -61,45 +61,47 @@ class HomePage(webapp2.RequestHandler):
         
         user = users.get_current_user()
         
-        meme = Meme(
-            line1=self.request.get('user-first-ln'), 
-            line2=self.request.get('user-second-ln'),
-            owner=user.nickname(),
-            img_choice=self.request.get('meme-type')
+        event = Event(
+            name=self.request.get('event-name'), 
+            short_description=self.request.get('short-description'),
+            date=self.request.get('event-date'),
+            age_range=self.request.get('age-range'),
+            location=self.request.get('event-location')
+            
         )
-        meme_key = meme.put()
-        self.response.write("Meme created: " + str(meme_key) + "<br>")
-        self.response.write("<a href='/allmemes'>All memes</a> | ")
-        self.response.write("<a href='/usermemes'>My memes</a>")
+        event_key = event.put()
+        self.response.write("Meme created: " + str(event_key) + "<br>")
+        self.response.write("<a href='/upcoming'>All Events</a> | ")
+        self.response.write("<a href='/user_events'>My events</a>")
         
 
 
-class AllMemesHandler(webapp2.RequestHandler):
+class UpcomingHandler(webapp2.RequestHandler):
     def get(self):
         checkLoggedInAndRegistered(self)
         
         
         
-        all_memes = Meme.query().fetch() #modify
+        all_events = Event.query().fetch() #modify
         
         the_variable_dict = {
-            "all_memes": all_memes
+            "all_memes": all_events
         }
         
-        all_memes_template = the_jinja_env.get_template('templates/all_memes.html')
+        all_memes_template = the_jinja_env.get_template('templates/upcoming.html')
         self.response.write(all_memes_template.render(the_variable_dict))
 
-class UserMemesHandler(webapp2.RequestHandler): #imporatnt line 137 keeps user stuff
+class UserEventsHandler(webapp2.RequestHandler): #imporatnt line 137 keeps user stuff
     def get(self):
         checkLoggedInAndRegistered(self)
         
         user = users.get_current_user()
         email_address = user.nickname()
         
-        user_memes = Meme.query().filter(Meme.owner == email_address).fetch() #modify get stored in user names
+        user_events = Event.query().filter(Event.owner == email_address).fetch() #modify get stored in user names
         
         the_variable_dict = {
-            "user_memes": user_memes
+            "user_memes": user_events
         }
         
         user_memes_template = the_jinja_env.get_template('templates/user_memes.html') #redirects
@@ -107,7 +109,14 @@ class UserMemesHandler(webapp2.RequestHandler): #imporatnt line 137 keeps user s
    
         
 
-class LoginHandler(webapp2.RequestHandler):
+class AddEventsHandler(webapp2.RequestHandler):
+    def get(self):
+        add_template = the_jinja_env.get_template('templates/add_events.html')
+        self.response.write(add_template.render())
+
+
+
+class SignInHandler(webapp2.RequestHandler):
     def get(self):
         
         login_template = the_jinja_env.get_template('templates/login.html') #this will be loaded first
@@ -118,7 +127,7 @@ class LoginHandler(webapp2.RequestHandler):
         self.response.write(login_template.render(the_variable_dict))
         
 
-class RegistrationHandler(webapp2.RequestHandler):
+class SignUpHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         
@@ -168,36 +177,36 @@ class RegistrationHandler(webapp2.RequestHandler):
     
     
     
->>>>>>> f73ee8a691b92dfb6bf2043bd1fec601d25e9d36
+
     
     def post(self):
         user = users.get_current_user()
         
         #Create a new CSSI User in our database
         
-        cssi_user = CssiUser(
+        event_user = Event_User(
             first_name=self.request.get('first_name'), 
             last_name =self.request.get('last_name'), 
             email=user.nickname() # modify for our use
         )
         
-        cssi_user.put()
+        Event_User.put()
         
         self.response.write('Thanks for signing up, %s! <br><a href="/">Home</a>' %
-        cssi_user.first_name)
+        event_user.first_name)
         
                   
     
 app = webapp2.WSGIApplication([
-<<<<<<< HEAD
-    ('/', HomeHandler),
-    ('/allmemes', AllMemesHandler), 
-    ('/usermemes', UserMemesHandler), #line 83
-    ('/login', LoginHandler), # should use this rdr
-    ('/register', RegistrationHandler)# should use this rdr
-], debug=True)
-=======
-    ('/', EnterInfoHandler)
+
+    ('/', HomePage),
+    ('/upcoming', UpcomingHandler),
+    ('/add_events', AddEventsHandler),
+    #('/maps', MapsHandler),
+#    ('/calendar' CalendarHandler),
+    ('/user_events', UserEventsHandler), #line 83
+    ('/sign_in', SignInHandler), # should use this rdr
+    ('/signup', SignUpHandler)# should use this rdr
 ], debug=True)
 
->>>>>>> f73ee8a691b92dfb6bf2043bd1fec601d25e9d36
+
