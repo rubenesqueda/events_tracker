8855402a1659bce5cdcd345c8ad1233a6ff0f43b
Ubuntu <ubuntu@ip-172-31-93-52.ec2.internal>
2019-08-08 01:46:10 +0000

    log in

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import webapp2
 import jinja2
 import os
-from models import Event, CssiUser 
+from models import Event, Event_User 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 the_jinja_env = jinja2.Environment(
@@ -12,23 +13,11 @@ the_jinja_env = jinja2.Environment(
 
 def checkLoggedInAndRegistered(request):
     # Check if user is logged in
-    
     user = users.get_current_user()
         
     if not user: 
         request.redirect("/sign_in") #ask to login,if not redirect 
-        return
-        welcome_template = the_jinja_env.get_template('templates/index.html')
-        self.response.write(welcome_template.render())
-    
-#class Upcoming(webapp2.RequestHandler):
-    
-    
-    
-    
-    
-#class AddEvents(webapp2.RequestHandler):
-
+        return 
     
     # Check if user is registered
        
@@ -40,20 +29,30 @@ def checkLoggedInAndRegistered(request):
          request.redirect("/signup") #useful if not registered goes to rdr
          return 
 
+
+def isRegistered():
+    user = users.get_current_user()
+        
+    # Check if user is registered
+       
+    email_address = user.nickname()
+    registered_user = Event_User.query().filter(Event_User.email == email_address).get() 
+    
+    return registered_user
     
 #class Map(webapp2.RequestHandler):
     
     
 
-class HomePage(webapp2.RequestHandler):
+class HomePageHandler(webapp2.RequestHandler):
     def get(self):  
-        checkLoggedInAndRegistered(self)
-        
+
         the_variable_dict = {
-            "logout_url":  users.create_logout_url('/')
+            "logout_url":  users.create_logout_url('/'),
+            "login_url": users.create_login_url('/signup')
         }
         
-        welcome_template = the_jinja_env.get_template('templates/home.html')
+        welcome_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(welcome_template.render(the_variable_dict))
 
     def post(self):
@@ -85,7 +84,7 @@ class UpcomingHandler(webapp2.RequestHandler):
         all_events = Event.query().fetch() #modify
         
         the_variable_dict = {
-            "all_memes": all_events
+            "all_events": all_events
         }
         
         all_memes_template = the_jinja_env.get_template('templates/upcoming.html')
@@ -101,10 +100,10 @@ class UserEventsHandler(webapp2.RequestHandler): #imporatnt line 137 keeps user 
         user_events = Event.query().filter(Event.owner == email_address).fetch() #modify get stored in user names
         
         the_variable_dict = {
-            "user_memes": user_events
+            "user_events": user_events
         }
         
-        user_memes_template = the_jinja_env.get_template('templates/user_memes.html') #redirects
+        user_memes_template = the_jinja_env.get_template('templates/user_events.html') #redirects
         self.response.write(user_memes_template.render(the_variable_dict))
    
         
@@ -118,10 +117,10 @@ class AddEventsHandler(webapp2.RequestHandler):
 
 class SignInHandler(webapp2.RequestHandler):
     def get(self):
-        
-        login_template = the_jinja_env.get_template('templates/login.html') #this will be loaded first
+        checkLoggedInAndRegistered(self)
+        login_template = the_jinja_env.get_template('templates/signin.html') #this will be loaded first
         the_variable_dict = {
-            "login_url":  users.create_login_url('/')
+            "login_url":  users.create_login_url('/signup')
         }
         
         self.response.write(login_template.render(the_variable_dict))
@@ -129,98 +128,80 @@ class SignInHandler(webapp2.RequestHandler):
 
 class SignUpHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        
-        registration_template = the_jinja_env.get_template('templates/registration.html') #rdr
+        if isRegistered():
+            self.redirect("/")
+            return
+            
         the_variable_dict = {
-            "email_address":  user.nickname()
+            "logout_url":  users.create_logout_url('/')
         }
         
-        self.response.write(registration_template.render(the_variable_dict))
-#class Calendar(webapp2)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-<<<<<<< HEAD
-    
-=======
->>>>>>> d76bc2c3b2c9470578fdf10d32dbcf5b905209d0
+        welcome_template = the_jinja_env.get_template('templates/registration.html')
+        self.response.write(welcome_template.render(the_variable_dict))
+        
+        
+        
     def post(self):
+        #checkLoggedInAndRegistered(self)
+        
         user = users.get_current_user()
-        
-        #Create a new CSSI User in our database
-        
         event_user = Event_User(
             first_name=self.request.get('first_name'), 
             last_name =self.request.get('last_name'), 
             email=user.nickname() # modify for our use
         )
         
-        Event_User.put()
+        event_user.put()
         
         self.response.write('Thanks for signing up, %s! <br><a href="/">Home</a>' %
         event_user.first_name)
+        # event = Event(
+        #     name=self.request.get('event-name'), 
+        #     date=self.request.get('event-date'),
+        #     location=self.request.get('event-location'),
+        #     description=self.request.get('short-description'),
+        #     age_range=self.request.get('age-range')
+        # )
+        # event_key = event.put()
+        # self.response.write("Meme created: " + str(event_key) + "<br>")
+        # self.response.write("<a href='/allmemes'>All memes</a> | ")
+        # self.response.write("<a href='/usermemes'>My memes</a>")
+    #     checkLoggedInAndRegistered(self)
+    #     user = users.get_current_user()
+    #     registration_template = the_jinja_env.get_template('templates/registration.html') #rdr
+    #     the_variable_dict = {
+    #         "email_address":  user.nickname()
+    #     }
+        
+    #     self.response.write(registration_template.render(the_variable_dict))
+    #     #class Calendar(webapp2)
+    
+    # def post(self):
+    #     user = users.get_current_user()
+        
+    #     #Create a new CSSI User in our database
+        
+    #     event_user =Event_User(
+    #         first_name=self.request.get('first_name'), 
+    #         last_name =self.request.get('last_name'), 
+    #         email=user.nickname() # modify for our use
+    #     )
+        
+    #     Event_User.put()
+        
+    #     self.response.write('Thanks for signing up, %s! <br><a href="/">Home</a>' %
+    #     event_user.first_name)
         
                   
     
 app = webapp2.WSGIApplication([
-<<<<<<< HEAD
-
-    ('/', HomePage),
+    ('/', HomePageHandler),
     ('/upcoming', UpcomingHandler),
     ('/add_events', AddEventsHandler),
     #('/maps', MapsHandler),
-#    ('/calendar' CalendarHandler),
     ('/user_events', UserEventsHandler), #line 83
     ('/sign_in', SignInHandler), # should use this rdr
     ('/signup', SignUpHandler)# should use this rdr
 ], debug=True)
 
 
-=======
-    ('/', HomePage),
-    ('/allmemes', AllMemesHandler), 
-    ('/usermemes', UserMemesHandler), #line 83
-    ('/login', LoginHandler), # should use this rdr
-    ('/register', RegistrationHandler),# should use this rdr
-    ('/', EnterInfoHandler)
-], debug=True)
-
->>>>>>> d76bc2c3b2c9470578fdf10d32dbcf5b905209d0
